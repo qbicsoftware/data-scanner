26c830b510bb9cbb299907b36b42131b9fd950ec
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2024-05-14 15:15:41 +0200

    Remove empty JD

--- src/main/java/life/qbic/data/processing/registration/ValidationException.java
+++ src/main/java/life/qbic/data/processing/registration/ValidationException.java
@@ -1,12 +1,5 @@
 package life.qbic.data.processing.registration;
 
-/**
- * <b><class short description - 1 Line!></b>
- *
- * <p><More detailed description - When to use, what it solves, etc.></p>
- *
- * @since <version tag>
- */
 public class ValidationException extends RuntimeException {
 
   private final ErrorCode errorCode;
